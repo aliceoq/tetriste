1a1a36233ee1f9f37e4417744985d94c174e613d
aliceoq <aoqb@cin.ufpe.br>
2021-03-12 00:04:17 -0300

    adicionando rotacao

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const ROWS = 10;
+const ROWS = 20;
 const COLS = 12;
 const SPAWN_X = 100;
 const SPAWN_Y = -50;
@@ -7,16 +7,21 @@ const SHAPES = [
   [[0, 0, 0], [0, 0, 1], [1, 1, 1]],
   [[0, 1, 1], [0, 1, 1], [0, 0, 0]],
   [[0, 0, 0], [0, 1, 0], [1, 1, 1]],
-  [[0, 0, 0], [0, 0, 0], [1, 1, 1]]
+  [[0, 0, 0], [1, 1, 1], [0, 0, 0]],
+  [[0, 0, 1], [0, 0, 1], [0, 0, 1]]
 ];
-const COLORS = ['orange', 'pink', 'blue', 'red', 'pink', 'red', 'purple']
+const COLORS = ['orange', 'hotpink', 'blue', 'red', 'mediumseagreen', 'red', 'blueviolet']
 
 var curr_screen = emptyScreen();
 var curr_x = SPAWN_X;
 var curr_y = SPAWN_Y;
+var curr_shape = SHAPES[0].map(function(arr) {
+  return arr.slice();
+});
 var previous_x = SPAWN_X;
 var previous_y = SPAWN_Y;
-var curr_shape = SHAPES[0];
+var previous_shape = SHAPES[0].slice();
+
 var curr_color = 'orange';
 var ctx = document.getElementById('canvas').getContext('2d');
 var flagNoMove = false;
@@ -44,6 +49,18 @@ document.addEventListener('keydown', (e) => {
     }
     flagNoMove = false;
   }
+  else if (e.code === "ArrowUp" && !flagNoMove) {
+    flagNoMove = true;
+    rotatePiece();
+    flagNoMove = false;
+  }
+  else if (e.code === "ArrowDown" && !flagNoMove) {
+    flagNoMove = true;
+    if (isPositionValid(curr_x/BLOCK_SIZE, curr_y/BLOCK_SIZE)) {
+      playerMove();
+    }
+    flagNoMove = false;
+  }
 });
 
 function init() {
@@ -58,7 +75,10 @@ function emptyScreen() {
 }
 
 function newPiece() {
-  curr_shape = SHAPES[Math.floor(Math.random() * 3)];
+  curr_shape = SHAPES[Math.floor(Math.random() * 4)].map(function(arr) {
+    return arr.slice();
+  });
+  previous_shape = curr_shape;
 
   aux = curr_color;
   while (aux == curr_color){
@@ -71,26 +91,14 @@ function newPiece() {
   previous_y = -100;
 
   if (isPositionValid(curr_x/BLOCK_SIZE, curr_y/BLOCK_SIZE)) {
-    window.setTimeout(draw, 500);
+    window.setTimeout(draw, 250);
   }
   else {
     document.getElementById('result').innerHTML = 'perdeu';
   }
 }
 
-function draw() {
-  if (checkLine()) {
-    previous_x = -100;
-    previous_y = -100;
-    console.log('voltei')
-
-    for (j = 0 ; j < COLS ; j++) {
-      console.log(curr_screen[j][ROWS - 1 ]);
-    }
-
-  }
-
-    
+function draw() { 
   flagNoMove = true;
 
   if (isPositionValid(curr_x/BLOCK_SIZE, curr_y/BLOCK_SIZE)) {
@@ -105,6 +113,7 @@ function draw() {
     window.setTimeout(draw, 500);
   }
   else {
+    checkLine();
     flagNoMove = false;
     newPiece();
   }
@@ -126,7 +135,7 @@ function clearShape() {
   
   for (i = 0 ; i < 3 ; i++) {
     for (j = 0 ; j < 3 ; j++) {
-      if (curr_shape[i][j] == '1' && posY >= 0) {
+      if (previous_shape[i][j] == '1' && posY >= 0) {
         curr_screen[posX/BLOCK_SIZE][posY/BLOCK_SIZE] = 0;
         ctx.clearRect(posX, posY, BLOCK_SIZE, BLOCK_SIZE);
       }
@@ -143,7 +152,7 @@ function clearShapeScreen() {
 
   for (i = 0 ; i < 3 ; i++) {
     for (j = 0 ; j < 3 ; j++) {
-      if (curr_shape[i][j] == '1' && posY >= 0) {
+      if (previous_shape[i][j] == '1' && posY >= 0) {
         curr_screen[posX][posY] = 0;
       }
       posX += 1;
@@ -159,7 +168,7 @@ function fillShapeScreen() {
 
   for (i = 0 ; i < 3 ; i++) {
     for (j = 0 ; j < 3 ; j++) {
-      if (curr_shape[i][j] == '1' && posY >= 0) {
+      if (previous_shape[i][j] == '1' && posY >= 0) {
         curr_screen[posX][posY] = 1;
       }
       posX += 1;
@@ -188,6 +197,7 @@ function drawShape() {
 }
 
 function isPositionValid(posX, posY) {
+  document.getElementById('result3').innerHTML = posY;
   let flag = true;
   let posX_aux = posX;
 
@@ -199,7 +209,10 @@ function isPositionValid(posX, posY) {
     for (j = 0 ; j < 3 && flag; j++) {
       if (curr_shape[i][j] == '1') {
         if (posX >= COLS || posY >= ROWS || posX < 0) flag = false;
-        else if (posY >= 0 && posX >= 0 && curr_screen[posX][posY] == 1) flag = false;
+        else if (posY >= 0 && posX >= 0 && curr_screen[posX][posY] == 1) {
+
+          flag = false;
+        }
       }
       posX += 1;
     }
@@ -216,25 +229,20 @@ function isPositionValid(posX, posY) {
 
   flagNoMove = false;
   
+  
   return flag;
 }
 
 function checkLine() {
-  let ota = false;
-  console.log('checando')
-  for (i = ROWS - 1 ; i >= 0 ; i--) {
+  for (i = 0 ; i < ROWS ; i++) {
     let flagOk = true;
     for (j = 0 ; j < COLS && flagOk; j++) {
       if (curr_screen[j][i] == 0) {
         flagOk = false;
       }
     }
-    if (flagOk) {
-      ota = true;
-      deleteLine(i);
-    }
+    if (flagOk) deleteLine(i);
   }
-  return ota;
 }
 
 function deleteLine(line) {
@@ -251,10 +259,55 @@ function deleteLine(line) {
     }
   }
 
-  var imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height-BLOCK_SIZE);
+  var imageData = ctx.getImageData(0, 0, ctx.canvas.width, BLOCK_SIZE*line);
   ctx.putImageData(imageData, 0, BLOCK_SIZE);
   // now clear the right-most pixels:
   ctx.clearRect(0, 0, ctx.canvas.width, BLOCK_SIZE);
+}
+
+function rotatePiece() {
+  
+  var aux_shape = curr_shape.map(function(arr) {
+    return arr.slice();
+  });
+
+  for (i = 0 ; i < 3 ; i++) {
+    for (j = 0 ; j < 3 ; j++) {
+      curr_shape[i][j] = aux_shape[j][2-i] ;
+    }
+  }
+
+  //curr_y -= BLOCK_SIZE;
+  previous_shape = aux_shape;
+  clearShapeScreen();
+  
+  if (isPositionValid(curr_x/BLOCK_SIZE, curr_y/BLOCK_SIZE)) {
+    //document.getElementById('result').innerHTML = curr_screen;
+    
+    playerMove();
+    previous_shape = curr_shape;  
+    window.clearTimeout(draw);
+  }
+  else {
+    fillShapeScreen();
+    document.getElementById('result').innerHTML = 'NOPE';
+    curr_shape = aux_shape;
+    previous_shape = curr_shape;
+    //playerMove();
+  }
+
+  //document.getElementById('result2').innerHTML = screenString();
+}
+
+function screenString() {
+  var string = "";
+  for (i = 0 ; i < ROWS ; i++) {
+    for (j = 0 ; j < COLS ; j++) {
+      string += curr_screen[j][i] + " ";
+    }
+    string += "<br>";
+  }
+  return string;
 }
 
 init();
