d3fa73c2b9c33672fa4bff03a060798c6209636b
Alice Oliveira <aoqb@cin.ufpe.br>
2022-09-03 09:18:23 -0300

    prettier e atualizando readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tetriste
+
+tetris feito com js e html/css
+
+clique [aqui](https://aliceoq.github.io/tetriste/index.html) para jogar
+
+<p align="center">
+  <img src="tetris.png" alt="tetris"/>
+</p>
--- index.js
+++ index.js
@@ -4,16 +4,52 @@ const SPAWN_X = 125;
 const SPAWN_Y = -25;
 const BLOCK_SIZE = 25;
 const SHAPES = [
-  [[0, 0, 0], [0, 0, 1], [1, 1, 1]],
-  [[0, 0, 0], [1, 0, 0], [1, 1, 1]],
-  [[1, 1], [1, 1]],
-  [[0, 0, 0], [0, 1, 0], [1, 1, 1]],
-  [[0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]],
-  [[0, 0, 0], [0, 1, 1], [1, 1, 0]],
-  [[0, 0, 0], [1, 1, 0], [0, 1, 1]]
+  [
+    [0, 0, 0],
+    [0, 0, 1],
+    [1, 1, 1],
+  ],
+  [
+    [0, 0, 0],
+    [1, 0, 0],
+    [1, 1, 1],
+  ],
+  [
+    [1, 1],
+    [1, 1],
+  ],
+  [
+    [0, 0, 0],
+    [0, 1, 0],
+    [1, 1, 1],
+  ],
+  [
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+  ],
+  [
+    [0, 0, 0],
+    [0, 1, 1],
+    [1, 1, 0],
+  ],
+  [
+    [0, 0, 0],
+    [1, 1, 0],
+    [0, 1, 1],
+  ],
 ];
 const LEVELMULTFACTOR = [1, 1, 2, 2, 3, 3, 4, 4, 5];
-const COLORS = ['#feb377', '#ff9595', '#6886c5', '#b590ca', '#a8d3da', '#75b79e', '#b8de6f'];
+const COLORS = [
+  '#feb377',
+  '#ff9595',
+  '#6886c5',
+  '#b590ca',
+  '#a8d3da',
+  '#75b79e',
+  '#b8de6f',
+];
 const ctx = document.getElementById('canvas').getContext('2d');
 
 var curr_delay = 700;
@@ -24,7 +60,7 @@ var curr_x = SPAWN_X;
 var curr_y = SPAWN_Y;
 var curr_line_count = 0;
 var curr_color = 'orange';
-var curr_shape = SHAPES[5].map(function(arr) {
+var curr_shape = SHAPES[5].map(function (arr) {
   return arr.slice();
 });
 
@@ -63,13 +99,13 @@ function restart() {
 }
 
 function newPiece() {
-  curr_shape = SHAPES[Math.floor(Math.random() * 7)].map(function(arr) {
+  curr_shape = SHAPES[Math.floor(Math.random() * 7)].map(function (arr) {
     return arr.slice();
   });
   previous_shape = curr_shape;
 
   aux = curr_color;
-  while (aux == curr_color){
+  while (aux == curr_color) {
     curr_color = COLORS[Math.floor(Math.random() * 7)];
   }
 
@@ -78,28 +114,27 @@ function newPiece() {
   previous_x = -100;
   previous_y = -100;
 
-  if (isPositionValid(curr_x/BLOCK_SIZE, curr_y/BLOCK_SIZE)) {
+  if (isPositionValid(curr_x / BLOCK_SIZE, curr_y / BLOCK_SIZE)) {
     window.setTimeout(draw, curr_delay);
-  }
-  else {
+  } else {
     document.getElementById('result').innerHTML = 'perdeu D:';
   }
 }
 
 document.addEventListener('keydown', (e) => {
   if (flagNoMove) return;
-  switch(e.key) {
-    case "ArrowLeft":
+  switch (e.key) {
+    case 'ArrowLeft':
       moveLeft();
       break;
-    case "ArrowRight":
+    case 'ArrowRight':
       moveRight();
       break;
-    case "ArrowUp":
+    case 'ArrowUp':
       rotateShape();
       break;
-    case "ArrowDown":
-      if (isPositionValid(curr_x/BLOCK_SIZE, curr_y/BLOCK_SIZE)) {
+    case 'ArrowDown':
+      if (isPositionValid(curr_x / BLOCK_SIZE, curr_y / BLOCK_SIZE)) {
         move();
         computeScore(1);
       }
@@ -110,15 +145,17 @@ document.addEventListener('keydown', (e) => {
 });
 
 function emptyScreen() {
-  return Array.from({ length: COLS}, () => Array(ROWS ).fill(0));
+  return Array.from({ length: COLS }, () => Array(ROWS).fill(0));
 }
 
-function draw() { 
-  if (!flagNoMove && isPositionValid(curr_x/BLOCK_SIZE, curr_y/BLOCK_SIZE)) {
+function draw() {
+  if (
+    !flagNoMove &&
+    isPositionValid(curr_x / BLOCK_SIZE, curr_y / BLOCK_SIZE)
+  ) {
     move();
     window.setTimeout(draw, curr_delay);
-  }
-  else if (!flagNoMove) {
+  } else if (!flagNoMove) {
     checkLine();
     newPiece();
   }
@@ -136,7 +173,7 @@ function move() {
 
 function moveRight() {
   if (curr_y - BLOCK_SIZE < 0) return;
-  if (isPositionValid(curr_x/BLOCK_SIZE + 1, curr_y/BLOCK_SIZE - 1)) {
+  if (isPositionValid(curr_x / BLOCK_SIZE + 1, curr_y / BLOCK_SIZE - 1)) {
     previous_x = curr_x;
     curr_y -= BLOCK_SIZE;
     curr_x += BLOCK_SIZE;
@@ -148,7 +185,7 @@ function moveRight() {
 
 function moveLeft() {
   if (curr_y - BLOCK_SIZE < 0) return;
-  if (isPositionValid(curr_x/BLOCK_SIZE - 1, curr_y/BLOCK_SIZE - 1)) {
+  if (isPositionValid(curr_x / BLOCK_SIZE - 1, curr_y / BLOCK_SIZE - 1)) {
     previous_x = curr_x;
     curr_y -= BLOCK_SIZE;
     curr_x -= BLOCK_SIZE;
@@ -161,11 +198,11 @@ function moveLeft() {
 function clearShape() {
   posX = previous_x;
   posY = previous_y;
-  
-  for (i = 0 ; i < previous_shape.length ; i++) {
-    for (j = 0 ; j < previous_shape[i].length ; j++) {
+
+  for (i = 0; i < previous_shape.length; i++) {
+    for (j = 0; j < previous_shape[i].length; j++) {
       if (previous_shape[i][j] == '1' && posY >= 0) {
-        curr_screen[posX/BLOCK_SIZE][posY/BLOCK_SIZE] = 0;
+        curr_screen[posX / BLOCK_SIZE][posY / BLOCK_SIZE] = 0;
         ctx.clearRect(posX, posY, BLOCK_SIZE, BLOCK_SIZE);
       }
       posX += BLOCK_SIZE;
@@ -178,11 +215,11 @@ function clearShape() {
 function drawShape() {
   posX = curr_x;
   posY = curr_y;
-  
-  for (i = 0 ; i < curr_shape.length ; i++) {
-    for (j = 0 ; j < curr_shape[i].length ; j++) {
+
+  for (i = 0; i < curr_shape.length; i++) {
+    for (j = 0; j < curr_shape[i].length; j++) {
       if (curr_shape[i][j] == '1' && posY >= 0) {
-        curr_screen[posX/BLOCK_SIZE][posY/BLOCK_SIZE] = 1;
+        curr_screen[posX / BLOCK_SIZE][posY / BLOCK_SIZE] = 1;
         ctx.fillStyle = curr_color;
         ctx.fillRect(posX, posY, BLOCK_SIZE, BLOCK_SIZE);
       }
@@ -194,34 +231,34 @@ function drawShape() {
 }
 
 function clearShapeMatrix() {
-  posX = previous_x/BLOCK_SIZE;
-  posY = previous_y/BLOCK_SIZE;
+  posX = previous_x / BLOCK_SIZE;
+  posY = previous_y / BLOCK_SIZE;
 
-  for (i = 0 ; i < previous_shape.length ; i++) {
-    for (j = 0 ; j < previous_shape[i].length ; j++) {
+  for (i = 0; i < previous_shape.length; i++) {
+    for (j = 0; j < previous_shape[i].length; j++) {
       if (previous_shape[i][j] == '1' && posY >= 0) {
         curr_screen[posX][posY] = 0;
       }
       posX += 1;
     }
     posY += 1;
-    posX = previous_x/BLOCK_SIZE;
+    posX = previous_x / BLOCK_SIZE;
   }
 }
 
 function fillShapeMatrix() {
-  posX = previous_x/BLOCK_SIZE;
-  posY = previous_y/BLOCK_SIZE;
+  posX = previous_x / BLOCK_SIZE;
+  posY = previous_y / BLOCK_SIZE;
 
-  for (i = 0 ; i < previous_shape.length ; i++) {
-    for (j = 0 ; j < previous_shape[i].length ; j++) {
+  for (i = 0; i < previous_shape.length; i++) {
+    for (j = 0; j < previous_shape[i].length; j++) {
       if (previous_shape[i][j] == '1' && posY >= 0) {
         curr_screen[posX][posY] = 1;
       }
       posX += 1;
     }
     posY += 1;
-    posX = previous_x/BLOCK_SIZE;;
+    posX = previous_x / BLOCK_SIZE;
   }
 }
 
@@ -233,31 +270,32 @@ function isPositionValid(posX, posY) {
 
   clearShapeMatrix();
 
-  for (i = 0 ; i < curr_shape.length && flag ; i++) {
-    for (j = 0 ; j < curr_shape[i].length && flag ; j++) {
+  for (i = 0; i < curr_shape.length && flag; i++) {
+    for (j = 0; j < curr_shape[i].length && flag; j++) {
       if (curr_shape[i][j] == '1') {
         if (posX >= COLS || posY >= ROWS || posX < 0) flag = false;
-        else if (posY >= 0 && posX >= 0 && curr_screen[posX][posY] == 1) flag = false;
+        else if (posY >= 0 && posX >= 0 && curr_screen[posX][posY] == 1)
+          flag = false;
       }
       posX += 1;
     }
     posY += 1;
     posX = posX_aux;
-  } 
+  }
 
   fillShapeMatrix();
 
   flagNoMove = false;
-  
+
   return flag;
 }
 
 function checkLine() {
   deletedLines = 0;
 
-  for (i = 0 ; i < ROWS ; i++) {
+  for (i = 0; i < ROWS; i++) {
     let flag = true;
-    for (j = 0 ; j < COLS && flag ; j++) {
+    for (j = 0; j < COLS && flag; j++) {
       if (curr_screen[j][i] == 0) {
         flag = false;
       }
@@ -269,29 +307,29 @@ function checkLine() {
     }
   }
 
-  computeScore(deletedLines*deletedLines*100);
+  computeScore(deletedLines * deletedLines * 100);
 }
 
 function deleteLine(line) {
-  for (i = line ; i > 0 ; i--) {
-    for (j = 0 ; j < COLS ; j++) {
-      curr_screen[j][i] = curr_screen[j][i-1];
+  for (i = line; i > 0; i--) {
+    for (j = 0; j < COLS; j++) {
+      curr_screen[j][i] = curr_screen[j][i - 1];
     }
   }
 
-  var imageData = ctx.getImageData(0, 0, ctx.canvas.width, BLOCK_SIZE*line);
+  var imageData = ctx.getImageData(0, 0, ctx.canvas.width, BLOCK_SIZE * line);
   ctx.putImageData(imageData, 0, BLOCK_SIZE);
   ctx.clearRect(0, 0, ctx.canvas.width, BLOCK_SIZE);
 }
 
 function rotateShape() {
-  var aux_shape = curr_shape.map(function(arr) {
+  var aux_shape = curr_shape.map(function (arr) {
     return arr.slice();
   });
 
-  for (i = 0 ; i < aux_shape.length ; i++) {
-    for (j = 0 ; j < aux_shape[i].length ; j++) {
-      curr_shape[i][j] = aux_shape[j][aux_shape.length-i-1] ;
+  for (i = 0; i < aux_shape.length; i++) {
+    for (j = 0; j < aux_shape[i].length; j++) {
+      curr_shape[i][j] = aux_shape[j][aux_shape.length - i - 1];
     }
   }
 
@@ -299,13 +337,15 @@ function rotateShape() {
   clearShapeMatrix();
 
   curr_y -= BLOCK_SIZE;
-  if (curr_y >= 0 && isPositionValid(curr_x/BLOCK_SIZE, curr_y/BLOCK_SIZE)) {
+  if (
+    curr_y >= 0 &&
+    isPositionValid(curr_x / BLOCK_SIZE, curr_y / BLOCK_SIZE)
+  ) {
     move();
-    previous_shape = curr_shape;  
+    previous_shape = curr_shape;
 
     window.clearTimeout(draw);
-  }
-  else {
+  } else {
     curr_y += BLOCK_SIZE;
     fillShapeMatrix();
 
@@ -315,7 +355,7 @@ function rotateShape() {
 }
 
 function computeScore(points) {
-  curr_score = curr_score + points*LEVELMULTFACTOR[curr_level%8];
+  curr_score = curr_score + points * LEVELMULTFACTOR[curr_level % 8];
 
   document.getElementById('score').innerHTML = curr_score;
 
@@ -327,4 +367,3 @@ function computeScore(points) {
     document.getElementById('level').innerHTML = curr_level;
   }
 }
-
